7f05e25988fa40ae0f6983847d7ce89e31225933
Pablo <mppablomp@hotmail.com>
2022-07-14 23:58:18 -0300

    ver0.3

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+
+function App(){
+ return(
+    <div className="App">
+        <Navbar/>
+        <ItemListContainer greeting="RETRO GAMES FOREVER" />
+
+
+
+
+
+    </div>
+)
+
+
+}
+
+export default App;
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,19 @@
+import './StyleItemList.css';
+
+
+
+const ItemListContainer = (props)=>{
+
+
+    return (
+        <div className="Container-item">
+            <h1>{props.greeting}</h1>
+        </div>
+
+    )
+
+
+}
+
+
+export default ItemListContainer
