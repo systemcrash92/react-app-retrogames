05291d6dfba52c15016a77f9c0737e81e3777ded
Pablo <mppablomp@hotmail.com>
2022-07-18 20:27:41 -0300

    ver04

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
-import Navbar from "./components/Navbar/Navbar";
+
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import Navbar from "./components/Navbar/Navbar";
 
 function App(){
+    
+
  return(
     <div className="App">
-        <Navbar/>
-        <ItemListContainer greeting="RETRO GAMES FOREVER" />
+        <Navbar />
+       <ItemListContainer  greeting="RETRO GAMES FOREVER" />
+       
 
 
 
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Search.css'
+
+const Search = () =>{
+
+    return(
+
+        <div className="Search">
+
+            
+
+
+
+
+        </div>
+    )
+
+
+
+
+}
